1c258ec6ece0d983a943e2f20731b3e98d0c03ea
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-04-11 22:42:53 -0400

    Added Heroku host

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,7 +1,8 @@
 from core.settings.development import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',
+                 'localhost', 'https://engmedapp.herokuapp.com']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CORS_ALLOWED_ORIGINS = [
