e55709b029b40e069124dd773a1ad615f588c92b
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-04-11 22:47:18 -0400

    Fixed Heroku host

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,7 +2,7 @@ from core.settings.development import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',
-                 'localhost', 'https://engmedapp.herokuapp.com']
+                 'localhost', 'engmedapp.herokuapp.com']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CORS_ALLOWED_ORIGINS = [
