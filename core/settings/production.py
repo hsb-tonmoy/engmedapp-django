eb237bc0f8bbd9a9f1cdc11d44bd264c83f660f7
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-04-11 23:55:03 -0400

    Added Heroku DB config

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,12 @@
 from core.settings.development import *
+import dj_database_url
+
+DATABASES = {
+
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',
