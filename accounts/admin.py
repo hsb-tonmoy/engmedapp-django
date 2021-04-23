9bdf11f40ae20e183da0d8b8746bc4779a5951c8
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-04-23 14:49:35 -0400

    Fixing user delete issue with outstanding token.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rest_framework_simplejwt import token_blacklist
 from django.contrib.auth.admin import UserAdmin
 from .models import Accounts, Profile
 
@@ -50,3 +51,14 @@ class ProfileAdminConfig(admin.ModelAdmin):
 
 admin.site.register(Accounts, AccountsAdminConfig)
 admin.site.register(Profile, ProfileAdminConfig)
+
+
+class OutstandingTokenAdmin(token_blacklist.admin.OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken,
+                    OutstandingTokenAdmin)
