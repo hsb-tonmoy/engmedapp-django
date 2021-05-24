010dd3c1c53aea38152dae00f71d7eb62ca017ac
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-05-24 11:52:18 -0400

    Fixing google login.

--- core/settings/production.py
+++ core/settings/production.py
@@ -23,3 +23,20 @@ CORS_ALLOW_CREDENTIALS = True
 USE_S3 = True
 
 REST_SOCIAL_OAUTH_ABSOLUTE_REDIRECT_URI = 'https://engmedapp.com/auth/social/google'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
