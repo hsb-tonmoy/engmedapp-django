6b418da421c2e7a8a8a34c3a77713fa325a0f3be
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-05-24 14:00:06 -0400

    Fixing social erros v1

--- core/settings/development.py
+++ core/settings/development.py
@@ -258,12 +258,13 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
-
 REST_SOCIAL_OAUTH_ABSOLUTE_REDIRECT_URI = 'http://localhost:5000/auth/social/google'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
