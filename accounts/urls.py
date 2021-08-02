4e21471c8e495e3d2bdbcf073c68ae71c1869f3d
hsb-tonmoy <hsb.tonmoy@gmail.com>
2021-08-02 15:25:05 -0400

    Added Djoser URLs.

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,18 +11,6 @@ from django.urls import path, re_path
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(
-        r"^o/(?P<provider>\S+)/$",
-        AuthorizationURL.as_view(),
-        name="auth-url",
-    ),
-    re_path(r'^social/(?:(?P<provider>[a-zA-Z0-9_-]+)/?)?$',
-            SocialJWTPairOnlyAuthView.as_view(),
-            name='login_social_jwt_pair'),
-    path('login/', CookieTokenObtainPairView.as_view(),
-         name='login'),
-    path('login/refresh/',
-         CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', BlacklistTokenUpdateView.as_view(),
          name='logout'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile')
--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,7 +24,7 @@ urlpatterns = [
     path('questions/', include('question_db.urls', namespace='question')),
     path('accounts/', include('accounts.urls', namespace='account')),
     path('media_lib/', include('media_lib.urls', namespace='media_lib')),
-    path('auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls')),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
